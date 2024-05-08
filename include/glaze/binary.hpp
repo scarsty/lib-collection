571ee75abc856b8b561ec5f2184ab5abf1d2769a
sty <scarsty@gmail.com>
2024-05-08 16:01:54 +0800

    remove glaze

--- include/glaze/binary.hpp
+++
@@ -1,9 +0,0 @@
-// Glaze Library
-// For the license information refer to glaze.hpp
-
-#pragma once
-
-#include "glaze/binary/header.hpp"
-#include "glaze/binary/ptr.hpp"
-#include "glaze/binary/read.hpp"
-#include "glaze/binary/write.hpp"
